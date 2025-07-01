671076b97effb3ee3dab2ff804641c64808e3db4
Shadrack-445 <shadrackomondi17@gmail.com>
2025-07-01 21:58:41 +0300

    variables1

---
+++ Week_1/day1.py
@@ -0,0 +1,47 @@
+# # Section 1
+
+# name = "Shadrack"
+
+# print(name)
+
+
+
+# # Section 2
+
+# s = 51
+# w = 17 
+
+# d = s / w
+
+# print(d)
+
+
+
+# # Section 3
+
+# age = 18
+
+# new = age + 10
+
+# months = new * 12
+
+# weeks = new * 52
+
+# print(age,new,months,weeks)
+
+# age = input("Enter your age:")
+
+# print("You are" + " " + age + " " + "years old")
+
+
+
+# Section 4
+
+PI = 3.142
+
+radius = input("radius:")
+radius = float(radius)
+
+area = PI * radius ** 2
+print("Your area is",area)
+
